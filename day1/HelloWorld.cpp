d7cd60dc29ece19eac565f426588cfb334142021
Mohammad Hj Hassan <35444902+MohammadHHassan@users.noreply.github.com>
2020-07-13 16:14:09 +0800

    Add files via upload

---
+++ day1/HelloWorld.cpp
@@ -0,0 +1,14 @@
+/* 	
+	Day 1
+	HelloWorld.c
+	First program; printing out Hello world!
+*/
+
+#include <stdio.h>		// Header file for I/O
+
+int main(void)			// Main function
+{
+	printf("Hello world!");		// Printing out Hello world!
+	
+	return 0;
+}
