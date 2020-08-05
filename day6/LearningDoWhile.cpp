c8d1570d0491e58ec4de3b485b8764bdfe77ae7c
Mohammad Hj Hassan <35444902+MohammadHHassan@users.noreply.github.com>
2020-08-05 14:54:04 +0800

    Add files via upload

---
+++ day6/LearningDoWhile.cpp
@@ -0,0 +1,23 @@
+// day6
+// LearningDoWhile.c
+
+#include <stdio.h>
+
+int main(void)
+{
+	double x;
+	
+	do
+	{
+		printf("\nPlease enter a negative number: ");
+		scanf(" %lf", &x);
+	}while(x>=0);
+	
+	printf("\nYour number is %g", x);
+	printf("\nThank you!");
+	
+	return 0;
+}
+
+
+
---
+++ day6/LearningWhile.cpp
@@ -0,0 +1,20 @@
+// day6
+// LearningWhile.c
+
+#include <stdio.h>
+
+int main(void)
+{
+	int k=1;
+	
+	while(k<=10)
+	{
+		printf("%d\n", 10*k);
+		k++;	// k = k+1
+	}
+	
+	return 0;
+}
+
+
+
